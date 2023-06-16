55e31462fdebe60cf5b088cbed3358572f7e7224
PETERCHM <pm9066806@gmail.com>
2023-06-16 15:10:32 +0300

    git commit 'commit'

--- reviewer/serializers.py
+++ reviewer/serializers.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from rest_framework import serializers
 from .models import User, Product, Review, Rating, Feedback, Comment, Report, Moderation, Notification
 
@@ -45,3 +46,43 @@ class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
         fields = '__all__'
+=======
+from rest_framework import serializers
+from .models import User,  Review, Rating, Feedback, Comment, Report, Moderation
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = '__all__'
+
+class FeedbackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Feedback
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+class ReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Report
+        fields = '__all__'
+
+class ModerationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Moderation
+        fields = '__all__'
+
+>>>>>>> Stashed changes
